27a19e20bb83677e7226ca7d1ff9040ae6b8eb06
Michael Alarcon <malarconii@yahoo.com>
2017-09-08 19:46:33 -0700

    Completed comparator that sorts alphabetically by state

--- src/org/launchcode/Main.java
+++ src/org/launchcode/Main.java
@@ -1,6 +1,7 @@
 package org.launchcode;
 
 import org.launchcode.comparators.NameComparator;
+import org.launchcode.comparators.StateComparator;
 
 import java.util.ArrayList;
 
@@ -11,7 +12,10 @@ public class Main {
         ArrayList<City> cities = CityData.loadData();
 
         // TODO - Use different comparators here
-        NameComparator comparator = new NameComparator();
+//        NameComparator comparator = new NameComparator();
+//        cities.sort(comparator);
+
+        StateComparator comparator = new StateComparator();
         cities.sort(comparator);
 
         printCities(cities);
